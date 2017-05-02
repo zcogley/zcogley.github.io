761d73ddb3b56233edadc27f22730c440eaabb78
Zac Cogley <zaccogley@gmail.com>
2017-05-01 19:31:42 -0500

    major updates to oggi

--- scripts/today.js
+++ scripts/today.js
@@ -5,6 +5,7 @@ var model = {
 
   events: [],
   completes: [],
+  upcoming: [],
 
 };
 
@@ -15,12 +16,26 @@ function render() {
 
   // render todo items
   model.events.forEach(function(event) {
+
+    var delButton = $('<button></button>')
+      .attr('class', 'btn btn-danger')
+      .click(function() {
+        var index = model.events.indexOf(event);
+        model.events.splice(index, 1);
+        render();
+      });
+
+    var trashSpan = $('<span></span')
+      .attr('class', 'glyphicon glyphicon-trash')
+      .attr('aria-hidden', 'true');
+
+    delButton.append(trashSpan);
+
     // creates each ToDo item element
     var eventElement = $('<li></li>')
-
     var buttonElement = $('<button></button>')
       .text(event.event)
-      .attr('class', 'btn btn-primary')
+      .attr('class', 'btn btn-default')
       .click(function() {
         var index = model.events.indexOf(event);
         var done = model.events.splice(index, 1);
@@ -28,31 +43,23 @@ function render() {
         render();
       });
 
-    eventElement.append(buttonElement);
+    // creates the 'done' checkmark
+    var checkButton = $('<button></button>')
+      .attr('class', 'btn btn-success')
+      .click(function() {
+        var index = model.events.indexOf(event);
+        var done = model.events.splice(index, 1);
+        model.completes.push(done[0]);
+        render();
+      });
 
-    // code for old way with checkboxes
-    //   .text(event.event + ' ');
-    // // creates a corresponding checkbox for removing event
-    // var radioElement = $('<input>').attr({
-    //   type: 'checkbox'
-    // });
-    //   radioElement.click(function() {
-    //     // function removes event on checkbox click
-    //
-    //     // gets the index of the event to add to completes
-    //     var index = model.events.indexOf(event);
-    //
-    //     // done array temporarily holds event removed
-    //     var done = model.events.splice(index, 1);
-    //
-    //     // adds done event to completes array
-    //     model.completes.push(done[0]);
-    //
-    //     render();
-    //   });
-    //
-    // eventElement.append(radioElement);
+    var checkSpan = $('<span></span>')
+      .attr('class', 'glyphicon glyphicon-ok')
+      .attr('aria-hidden', 'true');
 
+    checkButton.append(checkSpan);
+
+    eventElement.append(delButton, buttonElement, checkButton);
 
     $('#events').append(eventElement);
   });
@@ -70,32 +77,53 @@ function render() {
   model.completes.forEach(function(complete) {
     // creates each complete item element
     var completeElement = $('<li></li>')
+
+    var delButton = $('<button></button>')
+      .attr('class', 'btn btn-danger')
+      .click(function() {
+        var index = model.completes.indexOf(complete);
+        model.completes.splice(index, 1);
+        render();
+      });
+
+    var trashSpan = $('<span></span')
+      .attr('class', 'glyphicon glyphicon-trash')
+      .attr('aria-hidden', 'true');
+
+    delButton.append(trashSpan);
+
     var btnElement = $('<button></button>')
       .text(complete.event)
-      .attr('class', 'btn btn-danger')
+      .attr('class', 'btn btn-default')
       .click(function() {
         var idx = model.completes.indexOf(complete);
         var did = model.completes.splice(idx, 1);
         model.events.push(did[0]);
         render();
-
       })
-    completeElement.append(btnElement);
 
+    // creates the 'redo' glyphicon
+    var redoButton = $('<button></button>')
+      .attr('class', 'btn btn-success')
+      .click(function() {
+        var index = model.completes.indexOf(complete);
+        var done = model.completes.splice(index, 1);
+        model.events.push(done[0]);
+        render();
+      });
+    var redoSpan = $('<span></span>')
+      .attr('class', 'glyphicon glyphicon-refresh')
+      .attr('aria-hidden', 'true');
 
-    // // creates a corresponding checkbox for deleting event
-    // var radElement = $('<input>').attr({
-    //   type: 'checkbox'
-    // });
-    //
-    // completeElement.append(radElement);
+    redoButton.append(redoSpan);
+
+    completeElement.append(delButton, btnElement, redoButton);
 
     $('#completes').append(completeElement);
-
   });
 
   var clearButton = $('<button></button>')
-    .text('ToDone')
+    .text('Finito')
     .attr('class', 'btn')
     .click(function() {
       model.completes = [];
@@ -104,11 +132,67 @@ function render() {
   $('#clearall').empty();
   $('#clearall').append(clearButton);
 
+  // clears the upcoming area
+  $('#upcomings').empty();
+
+  // renders upcoming items
+  model.upcoming.forEach(function(upcoming) {
+
+    // creates the delete upcoming event button
+    var delButton = $('<button></button>')
+      .attr('class', 'btn btn-danger')
+      .click(function() {
+        var index = model.upcoming.indexOf(event);
+        model.upcoming.splice(index, 1);
+        render();
+      });
+
+    var trashSpan = $('<span></span')
+      .attr('class', 'glyphicon glyphicon-trash')
+      .attr('aria-hidden', 'true');
+
+    delButton.append(trashSpan);
+
+    // creates each upcoming item element
+    var upcomingElement = $('<li></li>')
+    var btnElement = $('<button></button>')
+      .text(upcoming.event)
+      .attr('class', 'btn btn-default')
+      .click(function() {
+        var idx = model.upcoming.indexOf(upcoming);
+        var did = model.upcoming.splice(idx, 1);
+        model.events.push(did[0]);
+        render();
+      });
+
+    // creates each add event to inbox button
+    var inboxButton = $('<button></button>')
+      .attr('class', 'btn btn-success')
+      .click(function() {
+        var index = model.upcoming.indexOf(upcoming);
+        var done = model.upcoming.splice(index, 1);
+        model.events.push(done[0]);
+        render();
+      });
+    var inboxSpan = $('<span></span>')
+      .attr('class', 'glyphicon glyphicon-inbox')
+      .attr('aria-hidden', 'true');
+
+    inboxButton.append(inboxSpan);
+
+    upcomingElement.append(delButton, btnElement, inboxButton);
+    $('#upcomings').append(upcomingElement);
+  });
+
 }
 
 // adds a new event
 function addNewEvent(event){
   model.events.push({event});
+}
+
+function addUpcomingEvent(event){
+  model.upcoming.push({event});
 }
 
 
@@ -120,7 +204,11 @@ $(document).ready(() => {
     model.currentEvent = $('#eventbox').val();
   });
 
-  // when the form is submitted
+  $('#comingupbox').on('input', () => {
+    model.currentEvent = $('#comingupbox').val();
+  })
+
+  // when the add event form is submitted
   $('#add-event-form').submit((evt) => {
     // don't refresh the page
     evt.preventDefault();
@@ -132,9 +220,27 @@ $(document).ready(() => {
     render();
   });
 
-  // Makes Events and Completes Sortable
-  var el = document.getElementById('events', 'completes');
+  // when the add upcoming event form is submitted
+  $('#add-upcoming-form').submit((evt) => {
+    // don't refresh the page
+    evt.preventDefault();
+
+    // add a new event from whatever typed
+    addUpcomingEvent(model.currentEvent);
+
+    // renders page
+    render();
+
+    // clears the eventbox and focuses it
+    $('#comingupbox').val('');
+    $('#comingupbox').focus();
+  });
+
+  // Makes Events and Completes and Upcoming Sortable
+  var el = document.getElementById('events');
   var sortable = Sortable.create(el);
   var ell = document.getElementById('completes');
   var sortable = Sortable.create(ell);
+  var elll = document.getElementById('upcomings');
+  var sortable = Sortable.create(elll);
 })
